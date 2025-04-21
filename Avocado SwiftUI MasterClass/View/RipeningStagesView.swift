a35a468aa7de0b0e18eb95208a2a16a7c81ace49
Aran Fononi <aaronfononi@gmail.com>
2025-04-22 03:16:09 +0330

    Add RipeningView to RipeningStagesView

--- Avocado SwiftUI MasterClass/View/RipeningStagesView.swift
+++ Avocado SwiftUI MasterClass/View/RipeningStagesView.swift
@@ -8,8 +8,24 @@
 import SwiftUI
 
 struct RipeningStageView: View {
+    var ripeningStages : [Ripening] = ripeningData
     var body: some View {
-        Text("RipeningStage")
+        ScrollView(.horizontal, showsIndicators: false) {
+            VStack {
+                Spacer()
+                
+                HStack(alignment: .center, spacing: 25) {
+                    ForEach(ripeningStages) { ripening in
+                        RipeningView(ripening: ripening)
+                    } //: Loop
+                } //: HStack
+                .padding(.vertical)
+                .padding(.horizontal, 25)
+                Spacer()
+                
+            } //: VStack
+        } //: Scroll
+        .ignoresSafeArea(.all)
     }
 }
 
