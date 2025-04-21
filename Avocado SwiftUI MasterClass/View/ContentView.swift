dc4e4dfd0fd82cf61fdd66941e5a65306a539041
Aran Fononi <aaronfononi@gmail.com>
2025-04-21 23:36:53 +0330

    Create Footer inside ContentView and Add Header To it

--- Avocado SwiftUI MasterClass/View/ContentView.swift
+++ Avocado SwiftUI MasterClass/View/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 20) {
+                // MARK: - Header
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(alignment: .top, spacing: 0) {
+                        HeaderView()
+                    } //: HStack
+                } //: Scroll
+                
+                // MARK: - Footer
+                VStack(alignment: .center, spacing: 20) {
+                    Text("All About Avocados")
+                        .font(.system(.title, design: .serif))
+                        .fontWeight(.bold)
+                        .foregroundStyle(.colorGreenAdaptive)
+                        .padding(8)
+                    Text("Everything you wanted to know about avocados, but were too afraid to ask.")
+                        .font(.system(.body, design: .serif))
+                        .multilineTextAlignment(.center)
+                        .foregroundStyle(.gray)
+                        .padding(8)
+                } //: VStack
+                .frame(maxWidth: 640)
+                .padding()
+                .padding(.bottom, 85)
+            } //: VStack
+        } //: ScrollView
+        .ignoresSafeArea(edges: .all)
     }
 }
 
