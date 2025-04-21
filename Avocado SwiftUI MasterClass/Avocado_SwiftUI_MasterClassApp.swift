2e04f1b4c26e5819ee09ed01449fb6311eff0b25
Aran Fononi <aaronfononi@gmail.com>
2025-04-21 14:54:09 +0330

    Initial Commit

---
+++ Avocado SwiftUI MasterClass/Avocado_SwiftUI_MasterClassApp.swift
@@ -0,0 +1,17 @@
+//
+//  Avocado_SwiftUI_MasterClassApp.swift
+//  Avocado SwiftUI MasterClass
+//
+//  Created by Aran Fononi on 21/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Avocado_SwiftUI_MasterClassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
