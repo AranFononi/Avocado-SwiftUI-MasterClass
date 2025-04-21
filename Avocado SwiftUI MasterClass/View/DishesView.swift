5bf9ad23ef971398fb916d060a3ae2c826ce24a9
Aran Fononi <aaronfononi@gmail.com>
2025-04-22 00:09:32 +0330

    Complete DishesView

--- Avocado SwiftUI MasterClass/View/DishesView.swift
+++ Avocado SwiftUI MasterClass/View/DishesView.swift
@@ -9,12 +9,56 @@ import SwiftUI
 
 struct DishesView: View {
     var body: some View {
-        HStack {
-            DishesRowView(image: "icon-toasts", text: "Toasts", isTextLeft: false)
+        HStack(alignment: .center, spacing: 4) {
+            // Left Side
+            VStack(alignment: .leading, spacing: 4) {
+                DishesRowView(image: "icon-toasts", text: "Toasts", isTextLeft: false)
+                
+                Divider()
+                
+                DishesRowView(image: "icon-tacos", text: "Tacos", isTextLeft: false)
+                
+                Divider()
+                
+                DishesRowView(image: "icon-salads", text: "Salads", isTextLeft: false)
+                
+                Divider()
+                
+                DishesRowView(image: "icon-halfavo", text: "Spreads", isTextLeft: false)
+            } //: VStack
             
-            Image(systemName: "heart.circle")
+            // Center
+            VStack(alignment: .center, spacing: 16) {
+                HStack {
+                    Divider()
+                } //: HStack
+                
+                Image(systemName: "heart.circle")
+                    .font(.title)
+                    .fontWeight(.ultraLight)
+                    .imageScale(.large)
+                
+                HStack {
+                    Divider()
+                } //: HStack
+            } //: VStack
             
-            DishesRowView(image: "icon-guacamole", text: "Guacamole", isTextLeft: true)
+            // RightSide
+            VStack(alignment: .trailing, spacing: 4) {
+                DishesRowView(image: "icon-guacamole", text: "Guacamole", isTextLeft: true)
+                
+                Divider()
+                
+                DishesRowView(image: "icon-sandwiches", text: "Sandwich", isTextLeft: true)
+                
+                Divider()
+                
+                DishesRowView(image: "icon-soup", text: "Soup", isTextLeft: true)
+                
+                Divider()
+                
+                DishesRowView(image: "icon-smoothies", text: "Smoothie", isTextLeft: true)
+            } //: VStack
             
         } //: HStack
         .font(.system(.callout, design: .serif))
