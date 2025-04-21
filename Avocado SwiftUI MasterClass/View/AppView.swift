908ea377bd9731a639d97e94fb33b61b2038b4e3
Aran Fononi <aaronfononi@gmail.com>
2025-04-21 23:01:02 +0330

    Create TabView inside AppView using new SwiftUI Tab

--- Avocado SwiftUI MasterClass/View/AppView.swift
+++ Avocado SwiftUI MasterClass/View/AppView.swift
@@ -9,7 +9,25 @@ import SwiftUI
 
 struct AppView: View {
     var body: some View {
-        Text("AppView")
+        TabView {
+            Tab("Avocados", image: "tabicon-branch") {
+                AvocadosView()
+            } //: Avocados Tab
+            
+            Tab("Recipes", image: "tabicon-book") {
+                ContentView()
+            } //: Content Tab
+            
+            Tab("Ripening", image: "tabicon-avocado") {
+                RipeningStageView()
+            } //: Ripening Tab
+            
+            Tab("Settings", image: "tabicon-settings") {
+                SettingsView()
+            } //: Ripening Tab
+            
+        } //: TabView
+        .tint(.primary)
     }
 }
 
